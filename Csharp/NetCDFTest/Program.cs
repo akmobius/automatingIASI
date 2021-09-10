6745479d0f66740769de35404ed2ceb270d5267a
Markus Mobius <markusmobius@gmail.com>
2021-09-09 23:38:05 -0400

    added netcdf

---
+++ Csharp/NetCDFTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Research.Science.Data;
+using Newtonsoft.Json;
+
+namespace NetCDFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new Microsoft.Research.Science.Data.NetCDF4.NetCDFUri();
+            reader.FileName = "c:/users/mobius/downloads/W_XX-EUMETSAT-Darmstadt,HYPERSPECT+SOUNDING,METOPA+IASI_C_EUMP_20210904141453_77199_eps_o_l2.nc";
+            reader.OpenMode = ResourceOpenMode.ReadOnly;
+            Variable firstVar= null;
+            using (var ds = DataSet.Open(reader))
+            {
+                var varset = ds.Variables;
+                foreach(var v in varset)
+                {
+                    Console.WriteLine(v.Name);
+                    if (firstVar == null) { firstVar = v; }
+                }
+                //get details on first variable
+                Console.WriteLine($"Now let's print data for variable {firstVar.Name}:");
+                var d = firstVar.GetData();
+                Console.WriteLine(JsonConvert.SerializeObject(d));
+            }
+        }
+    }
+}
--- Csharp/README.txt
+++ Csharp/README.txt
@@ -1,2 +1,21 @@
 dotnet ef migrations add InitialCreate #builds the initial migration for entity framework
 dotnet ef database update #builds the database
+
+_______________________
+
+netcdf
+https://github.com/predictionmachines/SDSlite
+
+Windows
+For Windows download the version of netCDF4 (without DAP) corresponding 
+to your machine, either 32 or 64 bit. 
+These are currently: 
+https://www.unidata.ucar.edu/downloads/netcdf/ftp/netCDF4.7.4-NC4-32.exe 
+https://www.unidata.ucar.edu/downloads/netcdf/ftp/netCDF4.7.4-NC4-64.exe 
+When you install this library select the option to add its location 
+to your system PATH, so that SDSLite can find it.
+
+Linux
+For Linux install pre-built netCDF-C libraries. For example on Ubuntu:
+
+sudo apt-get install libnetcdf-dev
