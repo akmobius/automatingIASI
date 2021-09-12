2c3a02f488187e23502d7efc99c44d2a5eec9ac1
Ariel Mobius <arielmobius@gmail.com>
2021-09-12 14:02:27 -0400

    Fixes

---
+++ methane-website/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Header from './Components/header'
+import Table from './Components/table'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Table />
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ methane-website/src/Components/header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function header() {
+    return (
+        <div style={{fontSize: '30px'}}>
+            Pulling Methane Data from IASI
+        </div>
+    )
+}
+
+export default header
